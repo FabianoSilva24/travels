f8010dae754e0b59e24544fb49b58ba41992f560
fabianosilva24 <fabiano.jaragua24@hotmail.com>
2018-08-31 12:13:41 -0300

    mensagens de pedidos

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 app.use(bodyParser.urlencoded());
-app.use(expressMongoDb('mongodb://localhost/travels'));
 app.use(expressMongoDb('mongodb://<fabianosilva24>:<Twdahs100st>@ds241012.mlab.com:41012/travels'))
 
 app.get('', (req, res) => {
